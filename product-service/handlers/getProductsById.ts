7a95ffe046610d27aa8f660751d3678c19043afd
svetlanagorchanina <gsvetlana.gorchanina@gmail.com>
2020-10-31 20:22:23 +0300

    feat: Add error handlers to lambda functions

--- product-service/handlers/getProductsById.ts
+++ product-service/handlers/getProductsById.ts
@@ -1,19 +1,31 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
 import productList from "../data/productList.json";
+import { withCorsHeaders } from "../utils/withCorsHeaders";
 
 export const getProductsById: APIGatewayProxyHandler = async ({
   pathParameters,
 }) => {
-  const product = productList.find(({ id }) => id === pathParameters?.id);
+  try {
+    const product = productList.find(({ id }) => id === pathParameters.id);
 
-  return product
-    ? {
-        statusCode: 200,
-        body: JSON.stringify(product),
-      }
-    : {
-        statusCode: 404,
-        body: JSON.stringify({ message: "Product not found" }),
-      };
+    return withCorsHeaders(
+      product
+        ? {
+            statusCode: 200,
+            body: JSON.stringify(product),
+          }
+        : {
+            statusCode: 404,
+            body: JSON.stringify({ message: "Product not found" }),
+          }
+    );
+  } catch (e) {
+    return withCorsHeaders({
+      statusCode: 500,
+      body: JSON.stringify({
+        message: "Something went wrong. Please try again later.",
+      }),
+    });
+  }
 };
--- product-service/handlers/getProductsList.ts
+++ product-service/handlers/getProductsList.ts
@@ -1,11 +1,17 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import "source-map-support/register";
 import productList from "../data/productList.json";
+import { withCorsHeaders } from "../utils/withCorsHeaders";
 
-export const getProductsList: APIGatewayProxyHandler = async () => ({
-  statusCode: 200,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-  },
-  body: JSON.stringify(productList),
-});
+export const getProductsList: APIGatewayProxyHandler = async () =>
+  withCorsHeaders(
+    productList
+      ? {
+          statusCode: 200,
+          body: JSON.stringify(productList),
+        }
+      : {
+          statusCode: 404,
+          body: JSON.stringify({ message: "Product list not found" }),
+        }
+  );
