34e42b2ae51a3629a48c039c0cc7c57cdc1ab503
svetlanagorchanina <gsvetlana.gorchanina@gmail.com>
2020-11-15 16:09:07 +0300

    feat: Add importProductsFile lambda function

--- import-service/serverless.ts
+++ import-service/serverless.ts
@@ -1,44 +1,64 @@
-import type { Serverless } from 'serverless/aws';
+import type { Serverless } from "serverless/aws";
 
 const serverlessConfiguration: Serverless = {
   service: {
-    name: 'import-service',
-    // app and org for use with dashboard.serverless.com
-    // app: your-app-name,
-    // org: your-org-name,
+    name: "import-service",
   },
-  frameworkVersion: '2',
+  frameworkVersion: "2",
   custom: {
     webpack: {
-      webpackConfig: './webpack.config.js',
-      includeModules: true
-    }
+      webpackConfig: "./webpack.config.js",
+      includeModules: true,
+    },
   },
-  // Add the serverless-webpack plugin
-  plugins: ['serverless-webpack'],
+  plugins: ["serverless-webpack"],
   provider: {
-    name: 'aws',
-    runtime: 'nodejs12.x',
+    name: "aws",
+    runtime: "nodejs12.x",
+    region: "${self:provider.environment.REGION}",
+    stage: "dev",
     apiGateway: {
       minimumCompressionSize: 1024,
     },
     environment: {
-      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      BUCKET: "import-service-s3-dev",
+      REGION: "eu-west-1",
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
     },
+    iamRoleStatements: [
+      {
+        Effect: "Allow",
+        Action: "s3:ListBucket",
+        Resource: "arn:aws:s3:::${self:provider.environment.BUCKET}",
+      },
+      {
+        Effect: "Allow",
+        Action: "s3:*",
+        Resource: "arn:aws:s3:::${self:provider.environment.BUCKET}/*",
+      },
+    ],
   },
   functions: {
-    hello: {
-      handler: 'handler.hello',
+    importProductsFile: {
+      handler: "handler.importProductsFile",
       events: [
         {
           http: {
-            method: 'get',
-            path: 'hello',
-          }
-        }
-      ]
-    }
-  }
-}
+            method: "get",
+            path: "import",
+            request: {
+              parameters: {
+                querystrings: {
+                  name: true,
+                },
+              },
+            },
+            cors: true,
+          },
+        },
+      ],
+    },
+  },
+};
 
 module.exports = serverlessConfiguration;
