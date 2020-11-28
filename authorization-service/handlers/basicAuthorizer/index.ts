cf106bed867cf5ec317609f7d39da18cf887e090
svetlanagorchanina <gsvetlana.gorchanina@gmail.com>
2020-11-28 10:41:45 +0300

    feat: Init authorization service

---
+++ authorization-service/handlers/basicAuthorizer/index.ts
@@ -0,0 +1,14 @@
+export const basicAuthorizer = async (event, _context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message:
+          "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
