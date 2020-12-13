6a73ea8bc98032f4feb804796c74cf65608718d1
svetlanagorchanina <gsvetlana.gorchanina@gmail.com>
2020-12-13 13:53:51 +0300

    feat: Cache to GET products list response for 2 minutes

--- bff-service/src/proxy/proxy.controller.ts
+++ bff-service/src/proxy/proxy.controller.ts
@@ -1,9 +1,12 @@
 import {
   All,
+  CacheInterceptor,
   Controller,
+  Get,
   HttpException,
   HttpStatus,
   Req,
+  UseInterceptors,
 } from '@nestjs/common';
 import { Request } from 'express';
 import { ProxyService } from './proxy.service';
@@ -11,6 +14,15 @@ import { ProxyService } from './proxy.service';
 @Controller()
 export class ProxyController {
   constructor(private readonly proxyService: ProxyService) {}
+
+  @Get('product/products')
+  @UseInterceptors(CacheInterceptor)
+  cachedProductsProxy(@Req() request: Request) {
+    return this.proxyService.request(
+      `${process.env.product}/products`,
+      request,
+    );
+  }
 
   @All()
   proxy(@Req() request: Request) {
--- bff-service/src/proxy/proxy.module.ts
+++ bff-service/src/proxy/proxy.module.ts
@@ -1,10 +1,16 @@
-import { HttpModule, Module } from '@nestjs/common';
+import { CacheModule, HttpModule, Module } from '@nestjs/common';
 import { ProxyController } from './proxy.controller';
 import { ProxyService } from './proxy.service';
 import { ConfigModule } from '@nestjs/config';
 
+const CACHE_EXPIRATION_TIME = 2 * 60;
+
 @Module({
-  imports: [ConfigModule.forRoot(), HttpModule],
+  imports: [
+    ConfigModule.forRoot(),
+    HttpModule,
+    CacheModule.register({ ttl: CACHE_EXPIRATION_TIME }),
+  ],
   controllers: [ProxyController],
   providers: [ProxyService],
 })
