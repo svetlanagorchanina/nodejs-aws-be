b951a8830dcf2617c9c0982bad12851dcacea505
svetlanagorchanina <gsvetlana.gorchanina@gmail.com>
2020-12-13 11:17:02 +0300

    feat: Add proxy handlers

--- bff-service/src/proxy/proxy.controller.ts
+++ bff-service/src/proxy/proxy.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  All,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Req,
+} from '@nestjs/common';
+import { Request } from 'express';
 import { ProxyService } from './proxy.service';
 
 @Controller()
 export class ProxyController {
   constructor(private readonly proxyService: ProxyService) {}
 
-  @Get()
-  getHello(): string {
-    return this.proxyService.getHello();
+  @All()
+  proxy(@Req() request: Request) {
+    const [serviceName, ...apiPath] = request.url.split('/').filter(Boolean);
+    const apiEndpoint = process.env[serviceName];
+
+    if (!apiEndpoint) {
+      throw new HttpException('Cannot process request', HttpStatus.BAD_GATEWAY);
+    }
+
+    return this.proxyService.request(
+      `${apiEndpoint}/${apiPath.join('/')}`,
+      request,
+    );
   }
 }
--- bff-service/src/proxy/proxy.module.ts
+++ bff-service/src/proxy/proxy.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { ProxyController } from './proxy.controller';
 import { ProxyService } from './proxy.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), HttpModule],
   controllers: [ProxyController],
   providers: [ProxyService],
 })
